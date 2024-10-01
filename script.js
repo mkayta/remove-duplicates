99c2e42e51f3f8d06dee2e2c38d67c5eabfcf33a
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-01 20:12:51 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function removeDuplicates(copy){
+    let removing= new Set(copy);
+let makearray=Array.from(removing);
+return makearray;
+
+}
+console.log(removeDuplicates([1, 0, 1, 0])) 
+console.log(removeDuplicates(["New", "York", "City"])) 
+console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"])) 
